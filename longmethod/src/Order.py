dda9b43f69bd7178b5f91bc1e5fe32499e52af45
Oisin Hurley <oisin.hurley@gmail.com>
2026-06-16 19:32:46 +0100

    It works!

--- longmethod/src/Order.py
+++ longmethod/src/Order.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import List
+from typing import List, Literal
 
 
 @dataclass(frozen=True)
@@ -30,27 +30,46 @@ class Order:
         self.customer = customer
 
     def summarise(self) -> OrderSummary:
-        # Validation
-        if self.items is None:
-            raise ValueError("Items cannot be None")
-        if len(self.items) == 0:
-            raise ValueError("Order must contain items")
+        self.validate_items()
 
-        # Subtotal calculation
-        subtotal = sum(item.price * item.quantity for item in self.items)
+        subtotal = self.calculate_subtotal()
 
+        discount = self.calculate_discount(subtotal)
+
+        tax, taxable_amount = self.calculate_tax(discount, subtotal)
+
+        total = self.calculate_total(tax, taxable_amount)
+
+        return OrderSummary(subtotal, discount, tax, total)
+
+    def calculate_total(self, tax: float, taxable_amount: float) -> float:
+        # Total calculation
+        total = taxable_amount + tax
+        return total
+
+    def calculate_tax(self, discount: float, subtotal: float | Literal[0]) -> tuple[float, float]:
+        # Tax calculation
+        taxable_amount = subtotal - discount
+        tax = taxable_amount * 0.20
+        return tax, taxable_amount
+
+    def calculate_discount(self, subtotal: float | Literal[0]) -> float:
         # Discount rules
         discount = 0.0
         if self.customer.is_loyal():
             discount = subtotal * 0.10
         elif subtotal > 100:
             discount = subtotal * 0.05
+        return discount
 
-        # Tax calculation
-        taxable_amount = subtotal - discount
-        tax = taxable_amount * 0.20
+    def calculate_subtotal(self) -> float | Literal[0]:
+        # Subtotal calculation
+        subtotal = sum(item.price * item.quantity for item in self.items)
+        return subtotal
 
-        # Total calculation
-        total = taxable_amount + tax
-
-        return OrderSummary(subtotal, discount, tax, total)
+    def validate_items(self):
+        # Validation
+        if self.items is None:
+            raise ValueError("Items cannot be None")
+        if len(self.items) == 0:
+            raise ValueError("Order must contain items")
